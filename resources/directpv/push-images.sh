80600697115ade1a1e5633b7edb8e8b0f018c4d4
Bala FA <bala@minio.io>
2025-11-28 06:50:38 +0530

    DirectPV: Update v5.1.0 (#46)
    Signed-off-by: Bala.FA <bala@minio.io>

--- resources/directpv/push-images.sh
+++ resources/directpv/push-images.sh
@@ -53,11 +53,11 @@ function push_image() {
 }
 
 function main() {
-    push_image "quay.io/minio/csi-node-driver-registrar:v2.13.0-0" # quay.io/minio/csi-node-driver-registrar@sha256:8f00013a19bd3bea021f3e96b093814fb110b383c0fd050d2a5309afb0ed0ccb
-    push_image "quay.io/minio/csi-provisioner:v5.2.0-0" # quay.io/minio/csi-provisioner@sha256:24816a743663d153060f0c3fc30005f05bea23c8b0fd2551fd667042741e8562
-    push_image "quay.io/minio/livenessprobe:v2.15.0-0" # quay.io/minio/livenessprobe@sha256:d8f7d431a2a148970dcb337f24b265d173bcee58bbeeae9af7ae60f01ce49be2
-    push_image "quay.io/minio/csi-resizer:v1.13.1-0" # quay.io/minio/csi-resizer@sha256:fc0c1f9cbc0ebb16283c0e425c031041eedb0e8ebbe6a1adec3de41584548ce6
-    push_image "quay.io/minio/directpv:v5.0.2" # quay.io/minio/directpv@sha256:9c752ba828d005fcaa2dafa5a4cd69e873f159f44f0c2329d052e47e928a0a77
+    push_image "quay.io/minio/livenessprobe:v2.17.0-0" # quay.io/minio/livenessprobe@sha256:8f3b1bec9c87a832a3fe6e8b7f165e0ff048aef7373f9764f40efee456a00321
+    push_image "quay.io/minio/csi-node-driver-registrar:v2.15.0-0" # quay.io/minio/csi-node-driver-registrar@sha256:c571b1462c6798725c0da58aab4896f910b38dc4ef48352ead3e4625d2d63a06
+    push_image "quay.io/minio/csi-provisioner:v6.0.0-0" # quay.io/minio/csi-provisioner@sha256:fff8927753ef1a67278804897de5dda9fd47c48b27575d53daafb12ab7179446
+    push_image "quay.io/minio/csi-resizer:v2.0.0-0" # quay.io/minio/csi-resizer@sha256:0640655cdf10b17bf50b304d5c3555135141b6bd3d79260a3ce389bf90d4e4bf
+    push_image "quay.io/minio/directpv:v5.1.0" # quay.io/minio/directpv@sha256:76bb7e4d204f95fea9e05637f18b68de2e54543ed0e2f3c4eae330d72b304abb
 }
 
 init "$@"
